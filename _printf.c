30202f20596daed49750cc7ebe58ab823a722215
abu-bakarr <abangura14@yahoo.com>
2021-07-26 21:36:00 +0000

    printf added

--- _printf.c
+++ _printf.c
@@ -1,13 +1,53 @@
 #include "holberton.h"
+#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
+* _printf - prints a string with functionality
+* @format: the string to print
+*
+* Return: 0 Always success
+*/
+
+int _printf(const char *format, ...)
 {
-return (write(1, &c, 1));
+	int i, j, counter = 0;
+	va_list ap;
+	pt_t types[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"d", print_d},
+		{"%", print_p},
+		{"i", print_d},
+		{NULL, NULL},
+	};
+	if (format == NULL)
+		return (-1);
+	va_start(ap, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+			{
+				return (-1);
+			}
+			j = 0;
+			while (types[j].fs != NULL)
+			{
+				if (*(types[j].fs) == format[i])
+					counter += types[j].f(ap);
+				j++;
+			}
+			i++;
+		}
+		if (format[i] != '%' && format[i] != '\0')
+		{
+			counter += _putchar(format[i]);
+			i++;
+		}
+	}
+	va_end(ap);
+	return (counter);
 }
