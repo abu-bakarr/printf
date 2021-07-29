bde3fa0fce57bd723b1211490d401947de761433
abu-bakarr <abangura14@yahoo.com>
2021-07-29 09:50:12 +0000

    update made

---
+++ util_characters.c
@@ -0,0 +1,97 @@
+#include "holberton.h"
+/**
+ * print_percent - print percent
+ * Return: count
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
+/**
+** print_char - prints character
+** @arg: char to print
+** Return: 0 success
+**/
+int print_char(va_list arg)
+{
+	char i;
+
+	i = va_arg(arg, int);
+	_putchar(i);
+	return (1);
+}
+/**
+** print_string - prints string
+** @arg: string to print
+** Prints string
+** Return: string length
+**/
+int print_string(va_list arg)
+{
+	unsigned int i;
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+		str = "(null)";
+
+	for (i = 0; str[i]; i++)
+		_putchar(str[i]);
+
+	return (i);
+}
+/**
+ *print_rot13 - makes string to ROT13
+ *@arg: arg list put in
+ *Return: conversion
+ */
+int print_rot13(va_list arg)
+{
+	char *s;
+	char *temp;
+	int i, j;
+
+	s = va_arg(arg, char *);
+	if (s == NULL)
+		s = "(ahyy)";
+	temp = malloc(52 * sizeof(char));
+	if (temp == NULL)
+		return (0);
+
+	for (i = 0; s[i]; i++)
+	{
+		if ((s[i] >= 'a' && s[i] < 'n') || (s[i] >= 'A' && s[i] < 'N'))
+			temp[i] = s[i] + 13;
+		else if ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+			temp[i] = s[i] - 13;
+		else
+			temp[i] = s[i];
+	}
+
+	for (j = 0; j < i; j++)
+		_putchar(temp[j]);
+	free(temp);
+	return (j);
+}
+/**
+ * print_reverse - print string in reverse
+ * @arg: arguments list
+ * Return: count
+ */
+int print_reverse(va_list arg)
+{
+	int i, j;
+	char *s;
+
+	s = va_arg(arg, char *);
+
+	if (s == NULL)
+		s = ")llun(";
+	for (i = 0; s[i] != '\0'; i++)
+		;
+	for (j = (i - 1); j >= 0; j--)
+		_putchar(s[j]);
+	return (i);
+}
---
+++ util_numbers.c
@@ -0,0 +1,83 @@
+#include "holberton.h"
+/**
+ * print_number - print numbers
+ * @arg: va_list of arguments
+ * Return: number count
+ */
+int print_number(va_list arg)
+{
+	int maxint = 1000000000;
+	int minint = INT_MIN;
+	int number = va_arg(arg, int);
+	int i, count = 0, trigger = 0;
+	/* if negative and not max min int */
+	if (number < 0 && number > minint)
+	{
+		_putchar('-');
+		count++;
+		number *= -1;
+	}
+	if (number == 0)
+	{
+		_putchar('0');
+		count++;
+	}
+	if (number == minint) /* cut off lass number then convert to positive */
+	{
+		number = (number % (maxint / 10)) * -1;
+		_putchar('-');
+		count++;
+		trigger = 1;
+	}
+	for (i = 0; i < 10; i++) /* print digit by digit */
+	{
+		if (number / maxint != 0) /* loop till find left more digit */
+		{
+			_putchar(((number / maxint) % 10) + '0'); /* print digit*/
+			count++;
+		}
+		maxint /= 10; /* lower the divisior or go through the digits left to right*/
+	}
+	if (trigger == 1) /* if max min digt put back the last digit */
+	{
+		_putchar('8');
+		count++;
+	}
+	return (count);
+}
+/**
+ * print_binary - print in binary
+ * @arg: int arguments
+ * Return: number of arguments outputted
+ */
+int print_binary(va_list arg)
+{
+	unsigned int i = 0;
+	unsigned int j;
+	unsigned int number = va_arg(arg, unsigned int);
+	unsigned int counter = number;
+	unsigned int *remainder;
+
+	/* count number of characters (1 or 0) */
+	for (i = 1; counter != 0; i++)
+		counter /= 2;
+
+	/* allocating space to hold binary values + null check*/
+	remainder = malloc(i * sizeof(unsigned int));
+	if (remainder == NULL)
+		return (0);
+
+	/* adding remainders (binary into array backwards) */
+	for (i = 0; number != 0; i++)
+	{
+		remainder[i] = number % 2;
+		number /= 2;
+	}
+	remainder[i] = number; /*last binary added*/
+	/* printing remainders array starting from last number*/
+	for (j = i - 1; j > 0; j--)
+		_putchar(remainder[j] + '0');
+	_putchar(remainder[j] + '0'); /*print remainder[0]*/
+	free(remainder);
+	return (i);
+}
